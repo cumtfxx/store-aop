d6e90f07e8303c59e3ba3a8c54676e70ab826335
cumtfxx <283570872@qq.com>
2018-09-10 17:29:41 +0800

    sub

--- src/main/java/com/maker/store/aop/Aop.java
+++ src/main/java/com/maker/store/aop/Aop.java
@@ -1,26 +1,33 @@
 package com.maker.store.aop;
 
+import com.maker.store.StoreApplication;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
 
 @Aspect
 @Component
 public class Aop {
+    private Logger logger=LoggerFactory.getLogger(StoreApplication.class);
+
     @Pointcut("within(com.maker.store.controller.StoreController)")
     public  void pointcut(){
-
     }
 
     @Before("pointcut()")
     public void before(){
-        System.out.println("测试开始");
+        this.logger.info("开始时间："+new Date());
     }
 
     @After("pointcut()")
     public void after(){
-        System.out.println("测试结束");
+        this.logger.info("结束时间："+new Date());
     }
 }
